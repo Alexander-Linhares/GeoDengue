4ec1666c89018c6d12340afae7a2915e0a6188f5
Alexander Alcantara Linhares <alex.linharesalcantara@gmail.com>
2025-04-01 17:18:11 -0300

    Apagando codigo base inicial do main

--- src/main.py
+++ src/main.py
@@ -1,59 +1,7 @@
-import os
-import pandas as pd
-import flet as ft 
+from classes.futures.data_load_status import GeolocationLoadStatus
 
-#Essa função encontra o diretório raiz busca o diretório informado e procura os arquivos com sufixo informados
-#dirname: retorna o nome do diretório do caminho especificado
-#abspath: retorna o caminho absoluto de determinado subpath 
-#join: concatena de forma inteligente dois caminhos
-#__file__: possui o caminho do arquivo em execução
-def get_file_path(folder_name: str, file_sufix: str) -> list[str]:
-    root = os.path.dirname(os.path.abspath(__file__))
-
-    folder_path = os.path.join(root, folder_name)
-    if not os.path.exists(folder_path):
-        raise FileNotFoundError(f"O diretório {folder_name} não existe")
-
-    archives = os.listdir(folder_path)
-    if not archives:
-        print("Este diretório está vazio")
-
-    excel_files = [
-        full_path 
-        for file in archives if (
-            full_path := os.path.join(folder_path, file)) 
-                and file.endswith(file_sufix)
-    ]
-
-    return excel_files
-
-
-def main(page: ft.Page):
-    page.title = "File picker"
-
-    def pick_files_result(e: ft.FilePickerResultEvent):
-        print(e)
-    
-    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
-
-    page.overlay.append(pick_files_dialog)
-
-    page.add(
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(
-                    "Selecionar arquivo",
-                    icon=ft.Icons.UPLOAD_FILE,
-                    on_click=lambda _: pick_files_dialog.pick_files(
-                        dialog_title="Selecione uma planilha do excel da dengue ",
-                        allowed_extensions=["xlsx"],
-                        allow_multiple=True
-                    )
-                )
-            ]
-        )
-    )
+def main():
+    pass
 
 if __name__ == "__main__":
-    print("Olá mundo!")
-    ft.app(target=main)
+    pass
